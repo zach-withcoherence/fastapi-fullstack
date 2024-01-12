ba162b1076ae79bf312f9e99320a877c840f0afd
zach-withcoherence <86851918+zach-withcoherence@users.noreply.github.com>
2024-01-12 16:30:32 -0500

    WIP

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # postgres connection on heroku
-SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"].replace('postgres://', 'postgresql://')
 
 # mysql connection
 #SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
